c9009ff3997d09ee7e16d0b490c44c3cdcc4e3a5
EDUARDA DE JESUS SILVA <24122366@CFP127ALUNO.LOCAL>
2025-05-06 12:25:04 -0300

    tela home

---
+++ filmes.js
@@ -0,0 +1,9 @@
+
+async function buscar(){
+    const titulo = document.getElementById('titulo')
+    titulo.style.display = "none"
+}
+
+
+document.getElementById('lupa')
+    .addEventListener('click', buscar)
