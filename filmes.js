854685a40984a8091b4f789d4cee27148beb9abc
EDUARDA DE JESUS SILVA <24122366@CFP127ALUNO.LOCAL>
2025-05-13 12:29:12 -0300

    menu oculto

--- filmes.js
+++ filmes.js
@@ -3,7 +3,19 @@ async function buscar(){
     const titulo = document.getElementById('titulo')
     titulo.style.display = "none"
 }
+function entrarMenu(){
+    const menuOculto = document.getElementById('menuOculto')
+    const principal =  document.getElementById("principalInicial")
 
+    menuOculto.style.display="block"
+    
+}
+function sairMenu(){
+    const menuOculto = document.getElementById('menuOculto')
+    const principal =  document.getElementById("principalInicial")
 
+    menuOculto.style.display="none"
+    principal.style.width = "100%"
+}
 document.getElementById('lupa')
     .addEventListener('click', buscar)
